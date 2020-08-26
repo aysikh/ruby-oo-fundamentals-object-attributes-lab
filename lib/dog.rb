e4894d820f35c7d2a1b7edd21be6837b1123f68c
aysikh <amy.sikhammountry@gmail.com>
2020-08-26 08:43:50 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+    attr_accessor :name, :breed
+#     def initialize(dogs_name, dogs_breed)
+#         @name = dogs_name
+#         @breed = dogs_breed
+#     end
+end
+
+Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+    attr_accessor :name, :job
+    # def initialize(persons_name, occupation)
+    #     @name = persons_name
+    #     @job = occupation
+    # end
+end
+
+Person.new
